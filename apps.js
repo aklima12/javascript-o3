a55a1311668cfd0b7105b275ae0c455cb51009b9
aklima12 <128598790+aklima12@users.noreply.github.com>
2023-05-22 20:09:43 +0600

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,24 @@
+function 1st method
+function additionTwoNumber(x,y,z){
+	
+	var tatal=x+y+z;
+	return total;
+}
+
+console.log(additionTwoNumber(50,50,50))
+
+// //2nd method
+// function addintionTwoNumber(x,y,z){
+	
+	// var total=x,y,z;
+	// console.log(total)
+	
+}
+// additionTwoNumber(10,50,40)
+
+// function myfunction(){
+	// var num1=Number(document.getElementById('num1'));
+	// var num2=Number(document.getElementById('num2'));
+	// var total=num1.value+num2.value;
+	 document.write(total);
+// }
